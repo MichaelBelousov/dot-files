af3b1445128ae51e86e94a6aa75ed9b2e44975b3
Michael Belousov <michael.belousov98@gmail.com>
2018-08-15 11:38:28 -0400

    made gitstatgrep pep8 compliant

--- .scripts/gitstatgrep.py
+++ .scripts/gitstatgrep.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+import subprocess as subproc
+from argparse import ArgumentParser as ArgParse
+from typing import Dict, List, Any, cast
+import os
+
 """
 git status change types
 -m: modified
@@ -13,10 +18,6 @@ git status change types
 
 __author__ = 'Michael Belousov'
 
-import subprocess as subproc
-from argparse import ArgumentParser as ArgParse
-from typing import Dict, List, Any, cast
-import os
 
 change_types = {
     'M': 'modified',
@@ -25,33 +26,32 @@ change_types = {
     'R': 'renamed',
     '?': 'untracked',
     ' ': None
-    }
+}
 
-ChangeData = Dict[os.path.abspath, 
-        Dict({
-            'change': change_types.values(),
-            'is_staged': bool
-        })
-    ]
+ChangeData = Dict[os.path.abspath, Dict({
+        'change': change_types.values(),
+        'is_staged': bool
+    })
+]
+
 
 def parseStatus(status: str) -> Dict[os.path.abspath, Any]:
     """parse a git status output string to a queryable structure"""
     attrs = {}
     for line in status.split('\n'):
         # set staged to be if the first character holds the change type
-        staged      = line[0] not in (' ', '?')
-        change      = change_types(line[0 if staged else 1])
-        path        = line[2:].strip()
+        staged = line[0] not in (' ', '?')
+        change = change_types(line[0 if staged else 1])
+        path = line[2:].strip()
         attrs[path] = {
             'change': change,
             'is_staged': is_staged
         }
     return attrs
 
+
 if __name__ == '__main__':
     # TODO: use stream
     out = subproc.check_output('git status --porcelain')
     stat = parseStatus(out)
     print(out)
-
-
