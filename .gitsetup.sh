94682cdb1024ec98ddca92cde92a8344372a67d5
Michael Belousov <michael.belousov98@gmail.com>
2018-11-07 07:59:47 -0500

    added sudo abbrev and gitsetup script

---
+++ .gitsetup.sh
@@ -0,0 +1,6 @@
+
+# add conditional for cygwin windows vs real linux
+git config --global core.autocrlf input
+git config --global core.safecrlf false
+git config --global user.name "Michael Belousov"
+git config --global user.email "Michael.Belousov98@gmail.com"
